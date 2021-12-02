1cd74e7bc21d44c332f9b6c2b4d3843ec580f701
John <ewertonjohn8@gmail.com>
2021-12-01 22:03:57 -0300

    up

---
+++ src/app/shared/model/USUARIOS.ts
@@ -0,0 +1,20 @@
+export const USUARIOS = [
+  {
+    nome: 'Gustavo',
+    cpf: '123',
+    idade: 34,
+    telefone: '83986621310'
+  },
+  {
+    nome: 'Wagner',
+    cpf: '124',
+    idade: 32,
+    telefone: '8398755241602'
+  },
+  {
+    nome: 'Diniz',
+    cpf: '125',
+    idade: 30,
+    telefone: '8398855245902'
+  }
+];
